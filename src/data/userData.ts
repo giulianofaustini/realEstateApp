c1e38dfc74761178fb5ee1ab1db67e4aca218351
Giuliano Faustini <giuliperus@gmail.com>
2024-01-02 17:06:55 +0200

    create user model for DB

--- src/data/userData.ts
+++ src/data/userData.ts
@@ -1,27 +1,22 @@
-import { UserInterface } from '../interfaces/userInterface';
+import { UserInterface } from "../interfaces/userInterface";
 
-
-
-export const users: UserInterface[] = [ 
-    {
-        id: '1',
-        name: 'Matti Virtanen',
-        email: 'matti.virtanen@example.com',
-        password: 'password123',
-        phoneNumber: '123-456-789',
-        isAdmin: false,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-        {
-        id: '2',
-        name: 'Anna Koskinen',
-        email: 'anna.koskinen@example.com',
-        password: 'password456',
-        phoneNumber: '987-654-321',
-        isAdmin: true,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      }
-
-]
+export const users: UserInterface[] = [
+  {
+    id: "1",
+    username: "Matti Virtanen",
+    email: "matti.virtanen@example.com",
+    password: "password123",
+    isAdmin: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: "2",
+    username: "Anna Koskinen",
+    email: "anna.koskinen@example.com",
+    password: "password456",
+    isAdmin: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
--- src/interfaces/userInterface.ts
+++ src/interfaces/userInterface.ts
@@ -1,9 +1,9 @@
 export interface UserInterface {
     id: string;
-    name: string;
+    username: string;
     email: string;
     password: string;
-    phoneNumber: string;
+  
     isAdmin: boolean;
     createdAt: Date;
     updatedAt: Date;
