ac2282b27f4e0be921d506553cb31f618c33c1e3
Giuliano Faustini <giuliperus@gmail.com>
2024-01-08 17:29:46 +0200

    state from App passed down to houses for rent and houses for sale component. Refetch the lists every time the componet mounts, so that when user redirected to lists after creating house, lists are updated.

--- src/types.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace Express {
-    export interface Response {
-      cookie(name: string, value: any, options?: any): Response;
-    }
-  }
-  
--- src/utils/error.ts
+++
@@ -1,9 +0,0 @@
-interface CustomError extends Error {
-    statusCode?: number;
-  }
-  
-  export const errorHandler = (statusCode: number, message: string): CustomError => {
-    const err: CustomError = new Error(message);
-    err.statusCode = statusCode;
-    return err;
-  };
