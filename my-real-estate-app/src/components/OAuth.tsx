89ad99a27fcb8c74cff26ac92e38c7b99359c6c0
Giuliano Faustini <giuliperus@gmail.com>
2024-01-10 07:41:09 +0200

    google auth passed into currentUser from redux at sign in returning name, email, photo with url

--- my-real-estate-app/src/components/OAuth.tsx
+++ my-real-estate-app/src/components/OAuth.tsx
@@ -1,9 +1,18 @@
 import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import { app } from '../firebase';
+import { useDispatch } from 'react-redux';
+import { setCurrentUser } from  "../redux/user/userSlice"
+
 
 export const OAuth = () => {
 
 const handleGoogleClick = async () => {
+
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const dispatch = useDispatch();
+
+
     try {
 
        
@@ -11,8 +20,17 @@ const handleGoogleClick = async () => {
         const provider = new GoogleAuthProvider();
         const auth = getAuth(app)
         const result = await signInWithPopup(auth, provider)
-        console.log(result)
+        const res = await fetch('http://localhost:3000/api/auth/google', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name: result.user?.displayName, email: result.user?.email, photo: result.user?.photoURL })
+        })
+        const data = await res.json()
+        dispatch(setCurrentUser(data))
         
+
     } catch (error) {
         console.log('cannot sign in with googlwe')
         
