1b9ae12b06f942c1ff707ba8177831cc4893b40b
Giuliano Faustini <giuliperus@gmail.com>
2024-01-11 08:12:03 +0200

    uplaod in house for sale form on upload images

--- my-real-estate-app/src/pages/HouseForSaleForm.tsx
+++ my-real-estate-app/src/pages/HouseForSaleForm.tsx
@@ -1,106 +1,172 @@
-import React, { useState } from 'react'
-import { HouseInterface } from  "../../../src/interfaces/houseInterface"
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { UserState } from '../redux/user/userSlice';
-
-
+import React, { useState } from "react";
+import { HouseInterface } from "../../../src/interfaces/houseInterface";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { UserState } from "../redux/user/userSlice";
 
 export const HouseForSaleForm = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const navigate = useNavigate();
 
-    const [loading, setLoading] = useState<boolean>(false);
-    const navigate = useNavigate();
+  const { currentUser } = useSelector((state: { user: UserState }) => ({
+    currentUser: state.user.currentUser,
+  }));
 
-    const { currentUser } = useSelector((state: { user: UserState }) => ({
-        currentUser: state.user.currentUser,
-    }));
+  console.log("currentUser from the house for sale form", currentUser);
 
-    console.log('currentUser from the house for sale form', currentUser);
+  const [formDataForSale, setFormDataForSale] = useState<HouseInterface>({
+    title: "",
+    description: "",
+    price: 0,
+    address: "",
+    location: "",
+    imageUrl: "",
+    agent: "",
+    bedrooms: 0,
+    bathrooms: 0,
+    addedBy: currentUser?.username,
+  });
 
+  const handleFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormDataForSale({
+      ...formDataForSale,
+      [e.target.id]: e.target.value,
+    });
+  };
 
-    
-    const [ formDataForSale , setFormDataForSale ] = useState<HouseInterface>({
-        title: "",
-        description: "",
-        price: 0,
-        address: "",
-        location: "",
-        imageUrl: "",
-        agent: "",
-        bedrooms: 0,
-        bathrooms: 0,
-        addedBy: currentUser?.username,
-    })
+  const handleFormForSaleChange = async (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+    setLoading(true);
 
-    const handleFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    try {
+      const res = await fetch(
+        "http://localhost:3000/api/create-house-for-sale",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+          body: JSON.stringify(formDataForSale),
+        }
+      );
+
+      const data = await res.json();
+      console.log(
+        "data from the handle submit form to check what info I have",
+        data
+      );
+
+      if (data.ok) {
         setFormDataForSale({
-          ...formDataForSale,
-          [e.target.id]: e.target.value,
+          title: "",
+          description: "",
+          price: 0,
+          address: "",
+          location: "",
+          imageUrl: "",
+          agent: "",
+          bedrooms: 0,
+          bathrooms: 0,
         });
-      };
-
-
-      const handleFormForSaleChange = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        setLoading(true);
-
-        try {
-            const res = await fetch("http://localhost:3000/api/create-house-for-sale", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                credentials: "include",
-                body: JSON.stringify(formDataForSale),
-            });
-
-            const data = await res.json();
-            console.log('data from the handle submit form to check what info I have', data);
-
-            if (data.ok) {
-                setFormDataForSale({
-                    title: "",
-                    description: "",
-                    price: 0,
-                    address: "",
-                    location: "",
-                    imageUrl: "",
-                    agent: "",
-                    bedrooms: 0,
-                    bathrooms: 0,
-                });
-                setLoading(false);
-                console.log(data.message);
-                
-            } else {
-                setLoading(false);
-                alert(data.message);
-            }
-        } catch (error) {
-            console.log(error);
-            setLoading(false);
-        } finally {
-        navigate("/api/housesForSale");
+        setLoading(false);
+        console.log(data.message);
+      } else {
+        setLoading(false);
+        alert(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    } finally {
+      navigate("/api/housesForSale");
     }
-    }
-
+  };
 
   return (
     <div className="max-w-lg  mx-auto mt-10">
-        <form className='flex flex-col gap-3 m-5' onSubmit={handleFormForSaleChange}>
-            <input className='p-5 border rounded-lg' type="text" placeholder='title' id='title' onChange={handleFormChange}/>
-            <input className='p-5 border rounded-lg' type="text" placeholder='description' id='description' onChange={handleFormChange}/>
-            <input className='p-5 border rounded-lg' type="number" placeholder='price' id='price' onChange={handleFormChange}/>
-            <input className='p-5 border rounded-lg' type="text" placeholder='address' id='address' onChange={handleFormChange}/>
-            <input className='p-5 border rounded-lg' type="text" placeholder='location' id='location' onChange={handleFormChange}/>
-            <input className='p-5 border rounded-lg' type="text" placeholder='imageUrl' id='imageUrl' onChange={handleFormChange}/>
-            <input className='p-5 border rounded-lg' type="text" placeholder='agent' id='agent' onChange={handleFormChange}/>
-            <input className='p-5 border rounded-lg' type="text" placeholder='bedrooms' id='bedrooms' onChange={handleFormChange}/>
-            <input className='p-5 border rounded-lg' type="text" placeholder='bathrooms' id='bathrooms' onChange={handleFormChange}/>
-            <button className='p-5 border rounded-lg' disabled={loading}>Submit</button>
-        </form>
+      <form
+        className="flex flex-col gap-3 m-5"
+        onSubmit={handleFormForSaleChange}
+      >
+        <input
+          className="p-5 border rounded-lg"
+          type="text"
+          placeholder="title"
+          id="title"
+          onChange={handleFormChange}
+        />
+        <input
+          className="p-5 border rounded-lg"
+          type="text"
+          placeholder="description"
+          id="description"
+          onChange={handleFormChange}
+        />
+        <input
+          className="p-5 border rounded-lg"
+          type="number"
+          placeholder="price"
+          id="price"
+          onChange={handleFormChange}
+        />
+        <input
+          className="p-5 border rounded-lg"
+          type="text"
+          placeholder="address"
+          id="address"
+          onChange={handleFormChange}
+        />
+        <input
+          className="p-5 border rounded-lg"
+          type="text"
+          placeholder="location"
+          id="location"
+          onChange={handleFormChange}
+        />
 
 
+
+        <div className="flex items-center gap-2 ">
+          <input
+            className="p-5 border rounded-lg max-h-20"
+            type="file"
+            placeholder="imageUrl"
+            id="imageUrl"
+            onChange={handleFormChange}
+          />
+          <button type="button" className="p-3 border rounded-full max-h-20 uppercase" >upload image</button>
+        </div>
+
+
+
+        <input
+          className="p-5 border rounded-lg"
+          type="text"
+          placeholder="agent"
+          id="agent"
+          onChange={handleFormChange}
+        />
+        <input
+          className="p-5 border rounded-lg"
+          type="text"
+          placeholder="bedrooms"
+          id="bedrooms"
+          onChange={handleFormChange}
+        />
+        <input
+          className="p-5 border rounded-lg"
+          type="text"
+          placeholder="bathrooms"
+          id="bathrooms"
+          onChange={handleFormChange}
+        />
+        <button className="p-5 border rounded-lg" disabled={loading}>
+          Submit
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
