7191929484e5d2b31733c522d0cea2f8c53566d3
Giuliano Faustini <giuliperus@gmail.com>
2024-01-27 20:07:53 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -27,12 +27,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(cookieParser());
 
-app.use(cors(
-  {
-    origin: "http://localhost:5173",
-    credentials: true
-  }
-))
+app.use(cors({
+  origin: 'https://sharestate.onrender.com',
+  credentials: true 
+}));
+
 app.use(express.json());
 
 
