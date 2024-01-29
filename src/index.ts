59cde5984e532067112553ae3c734cef0d24a986
Giuliano Faustini <giuliperus@gmail.com>
2024-01-29 16:30:48 +0200

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,45 @@
 import express from "express";
-
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRouter from "./routes/authRoute";
 dotenv.config();
-import cookieParser from 'cookie-parser';
-
-
-
-
-
-
+import cookieParser from "cookie-parser";
 import allHousesRouter from "../src/routes/allHouses";
 import allHousesForRentRouter from "../src/routes/allHousesForRent";
-import allUsersRouter from "../src/routes/allUsers"
-import signInRoute from "../src/routes/signInRoute"
-import signinWithGoogleRoute from "../src/routes/signInWithGoogleRoute"
-import userRouter from "../src/routes/userRouter"
+import allUsersRouter from "../src/routes/allUsers";
+import signInRoute from "../src/routes/signInRoute";
+import signinWithGoogleRoute from "../src/routes/signInWithGoogleRoute";
+import userRouter from "../src/routes/userRouter";
 
-mongoose.connect(process.env.MONGO ?? "").then(() => {
-  console.log("Connected to MongoDB");
-}
-).catch(err => console.log("Error connecting to MongoDB", err));
 const app = express();
-const port = process.env.PORT || 3000;
-app.use(cookieParser());
-
-const allowedOrigins = ['https://sharestate.onrender.com', 'http://localhost:5173'];
-
-app.use(cors({
-  origin: allowedOrigins ,
-  credentials: true 
-}));
-
 app.use(express.json());
+app.use(cookieParser());
+const port = process.env.PORT || 3000;
+const allowedOrigins = [
+  "https://sharestate.onrender.com",
+  "http://localhost:5173",
+];
 
+mongoose
+  .connect(process.env.MONGO ?? "")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => console.log("Error connecting to MongoDB", err));
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Real Estate API!");
 });
 
-
-app.use(express.static('dist'));
+app.use(express.static("dist"));
 
 app.use("/api/housesForSale", allHousesRouter);
 
@@ -50,13 +47,13 @@ app.use("/api/housesForRent", allHousesForRentRouter);
 
 app.use("/api/users", allUsersRouter);
 
-app.use("/api/userHouses", userRouter )
+app.use("/api/userHouses", userRouter);
 
-app.use("/api/sign-up", authRouter );
+app.use("/api/sign-up", authRouter);
 
-app.use("/api/sign-in" , signInRoute )
+app.use("/api/sign-in", signInRoute);
 
-app.use("/api/auth/google" , signinWithGoogleRoute )
+app.use("/api/auth/google", signinWithGoogleRoute);
 
 app.use("/api/create-house-for-sale", allHousesRouter);
 
@@ -77,9 +74,3 @@ app.use("/api/update-house-for-rent", allHousesForRentRouter);
 app.listen(Number(port), "0.0.0.0", () => {
   console.log(`Server running at http://0.0.0.0:${port}`);
 });
-
-// // // all api s 
-// // http://localhost:3000/api/users
-
-// // http://localhost:3000/api/housesForRent
-// http://localhost:3000/api/housesForSale
